f212673033e60e9d7f3030d47b47e41ba20dd0b1
Jorge <jorgeappontes13@gmail.com>
2025-10-13 22:08:36 -0300

    sql

---
+++ risenglish (1).sql
@@ -0,0 +1,371 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 14/10/2025 às 03:07
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `risenglish`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `alunos_turmas`
+--
+
+CREATE TABLE `alunos_turmas` (
+  `id` int(11) NOT NULL,
+  `aluno_id` int(11) NOT NULL,
+  `turma_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `alunos_turmas`
+--
+
+INSERT INTO `alunos_turmas` (`id`, `aluno_id`, `turma_id`) VALUES
+(17, 12, 5),
+(22, 12, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `arquivos_visiveis`
+--
+
+CREATE TABLE `arquivos_visiveis` (
+  `id` int(11) NOT NULL,
+  `aula_id` int(11) NOT NULL,
+  `conteudo_id` int(11) NOT NULL,
+  `visivel` tinyint(1) NOT NULL DEFAULT 1,
+  `data_modificacao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `aulas`
+--
+
+CREATE TABLE `aulas` (
+  `id` int(11) NOT NULL,
+  `titulo_aula` varchar(255) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `data_aula` date NOT NULL,
+  `horario` time NOT NULL,
+  `turma_id` int(11) NOT NULL,
+  `professor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `aulas`
+--
+
+INSERT INTO `aulas` (`id`, `titulo_aula`, `descricao`, `data_aula`, `horario`, `turma_id`, `professor_id`) VALUES
+(16, 'teste', 'relationship', '2025-10-14', '15:00:00', 5, 9),
+(17, 'aaaaa', 'edd', '2025-10-06', '00:00:00', 5, 9),
+(18, 'aaa', '', '2025-10-14', '00:00:00', 5, 9),
+(19, 'sss', '', '2025-10-14', '00:00:00', 5, 9),
+(20, 'sss', '', '2025-10-14', '00:00:00', 5, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `aulas_conteudos`
+--
+
+CREATE TABLE `aulas_conteudos` (
+  `id` int(11) NOT NULL,
+  `aula_id` int(11) NOT NULL,
+  `conteudo_id` int(11) NOT NULL,
+  `planejado` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `aulas_conteudos`
+--
+
+INSERT INTO `aulas_conteudos` (`id`, `aula_id`, `conteudo_id`, `planejado`) VALUES
+(48, 16, 34, 1),
+(49, 18, 34, 1),
+(50, 17, 34, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `conteudos`
+--
+
+CREATE TABLE `conteudos` (
+  `id` int(11) NOT NULL,
+  `professor_id` int(11) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `tipo_arquivo` varchar(50) NOT NULL,
+  `caminho_arquivo` varchar(255) NOT NULL,
+  `data_upload` timestamp NOT NULL DEFAULT current_timestamp(),
+  `eh_subpasta` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `conteudos`
+--
+
+INSERT INTO `conteudos` (`id`, `professor_id`, `parent_id`, `titulo`, `descricao`, `tipo_arquivo`, `caminho_arquivo`, `data_upload`, `eh_subpasta`) VALUES
+(34, 9, NULL, 'relationship', 'teste', 'TEMA', '', '2025-10-13 15:00:37', 0),
+(35, 9, 34, 'contrato', 'contrato (Arquivo: CONTRATO_Laura.pdf)', 'application/pdf', 'uploads/conteudos/1760367715_68ed1463a6238.pdf', '2025-10-13 15:01:55', 0),
+(36, 9, 34, 'video', 'video (Link: youtu.be)', 'URL', 'https://youtu.be/-moW9jvvMr4?si=p_dQzfxi-nh2X4BS', '2025-10-13 15:02:40', 0),
+(37, 9, NULL, 'teste', '', 'TEMA', '', '2025-10-13 15:13:35', 0),
+(38, 9, 34, 'basico', '', 'SUBPASSA', '', '2025-10-14 00:50:07', 1),
+(39, 9, 34, 'intermediario', '', 'SUBPASSA', '', '2025-10-14 00:50:17', 1),
+(40, 9, 38, 'video teste', 'video teste (Link: youtu.be)', 'URL', 'https://youtu.be/zctKiN-okXI?si=qS7R6qef2hQ0wDSM', '2025-10-14 00:51:01', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `recursos_uteis`
+--
+
+CREATE TABLE `recursos_uteis` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `data_criacao` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `recursos_uteis`
+--
+
+INSERT INTO `recursos_uteis` (`id`, `titulo`, `link`, `descricao`, `data_criacao`) VALUES
+(5, 'Linguee', 'https://www.linguee.com.br/', 'dicionario recomendado', '2025-10-09 22:40:41');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `turmas`
+--
+
+CREATE TABLE `turmas` (
+  `id` int(11) NOT NULL,
+  `nome_turma` varchar(100) NOT NULL,
+  `professor_id` int(11) DEFAULT NULL,
+  `inicio_turma` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `turmas`
+--
+
+INSERT INTO `turmas` (`id`, `nome_turma`, `professor_id`, `inicio_turma`) VALUES
+(5, 'Jorge', 9, '0000-00-00'),
+(6, 'turma 1', 9, '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo_usuario` enum('admin','professor','aluno') NOT NULL,
+  `informacoes` text DEFAULT NULL,
+  `reset_token` varchar(64) DEFAULT NULL,
+  `token_expira_em` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo_usuario`, `informacoes`, `reset_token`, `token_expira_em`) VALUES
+(1, 'Admin Risenglish', 'admin@risenglish.com', '$2y$10$/43jnz3JO8o5umNcabQ16eXnt1.pVdef3L7.6HvaILzUYbrRqPhBS', 'admin', NULL, NULL, NULL),
+(9, 'Laura', 'laura@risenglish.com', '$2y$10$L5r6urNbE7tLtiPzZFY/cuIAd65is7jtDcNVTIO/.YkmOsWnt.082', 'professor', NULL, NULL, NULL),
+(12, 'Jorge Augusto Possani Pontes', 'jorgeappontes13@gmail.com', '$2y$10$MBlYgYjHcew4oWv08mCSNuIJtbX05eexbZaEQ9w3yru3053kzArdm', 'aluno', 'teste', NULL, NULL);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `alunos_turmas`
+--
+ALTER TABLE `alunos_turmas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `aluno_id` (`aluno_id`,`turma_id`),
+  ADD KEY `turma_id` (`turma_id`);
+
+--
+-- Índices de tabela `arquivos_visiveis`
+--
+ALTER TABLE `arquivos_visiveis`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `aula_conteudo_unique` (`aula_id`,`conteudo_id`),
+  ADD KEY `aula_id` (`aula_id`),
+  ADD KEY `conteudo_id` (`conteudo_id`);
+
+--
+-- Índices de tabela `aulas`
+--
+ALTER TABLE `aulas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `turma_id` (`turma_id`),
+  ADD KEY `professor_id` (`professor_id`);
+
+--
+-- Índices de tabela `aulas_conteudos`
+--
+ALTER TABLE `aulas_conteudos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `aula_id` (`aula_id`,`conteudo_id`),
+  ADD KEY `conteudo_id` (`conteudo_id`);
+
+--
+-- Índices de tabela `conteudos`
+--
+ALTER TABLE `conteudos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `professor_id` (`professor_id`);
+
+--
+-- Índices de tabela `recursos_uteis`
+--
+ALTER TABLE `recursos_uteis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `turmas`
+--
+ALTER TABLE `turmas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `professor_id` (`professor_id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `idx_reset_token` (`reset_token`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `alunos_turmas`
+--
+ALTER TABLE `alunos_turmas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT de tabela `arquivos_visiveis`
+--
+ALTER TABLE `arquivos_visiveis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `aulas`
+--
+ALTER TABLE `aulas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT de tabela `aulas_conteudos`
+--
+ALTER TABLE `aulas_conteudos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT de tabela `conteudos`
+--
+ALTER TABLE `conteudos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT de tabela `recursos_uteis`
+--
+ALTER TABLE `recursos_uteis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `turmas`
+--
+ALTER TABLE `turmas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `alunos_turmas`
+--
+ALTER TABLE `alunos_turmas`
+  ADD CONSTRAINT `alunos_turmas_ibfk_1` FOREIGN KEY (`aluno_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `alunos_turmas_ibfk_2` FOREIGN KEY (`turma_id`) REFERENCES `turmas` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `arquivos_visiveis`
+--
+ALTER TABLE `arquivos_visiveis`
+  ADD CONSTRAINT `arquivos_visiveis_ibfk_1` FOREIGN KEY (`aula_id`) REFERENCES `aulas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `arquivos_visiveis_ibfk_2` FOREIGN KEY (`conteudo_id`) REFERENCES `conteudos` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `aulas`
+--
+ALTER TABLE `aulas`
+  ADD CONSTRAINT `aulas_ibfk_1` FOREIGN KEY (`turma_id`) REFERENCES `turmas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `aulas_ibfk_2` FOREIGN KEY (`turma_id`) REFERENCES `turmas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `aulas_ibfk_3` FOREIGN KEY (`professor_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `aulas_conteudos`
+--
+ALTER TABLE `aulas_conteudos`
+  ADD CONSTRAINT `aulas_conteudos_ibfk_1` FOREIGN KEY (`aula_id`) REFERENCES `aulas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `aulas_conteudos_ibfk_2` FOREIGN KEY (`conteudo_id`) REFERENCES `conteudos` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `conteudos`
+--
+ALTER TABLE `conteudos`
+  ADD CONSTRAINT `conteudos_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `turmas`
+--
+ALTER TABLE `turmas`
+  ADD CONSTRAINT `turmas_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
