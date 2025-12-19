16c44a78c64cbc769f1eca2ad1c3c360fc3f28e1
Jorge <jorgeappontes13@gmail.com>
2025-12-18 22:11:43 -0300

    anotacoes

--- php/aluno/recomendacoes.php
+++ php/aluno/recomendacoes.php
@@ -167,6 +167,7 @@ $recursos = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <a href="dashboard.php" class="rounded"><i class="fas fa-home"></i>&nbsp;&nbsp;Dashboard</a>
                     <a href="minhas_aulas.php" class="rounded"><i class="fas fa-calendar-alt"></i>&nbsp;&nbsp;&nbsp;Minhas Aulas</a>
                     <a href="recomendacoes.php" class="rounded active"><i class="fas fa-lightbulb"></i>&nbsp;&nbsp;&nbsp;Recomendações</a>
+                    <a href="anotacoes.php" class="rounded"><i class="fas fa-book-open"></i>&nbsp;&nbsp;&nbsp;Anotações</a>
                 </div>
 
                 <!-- Botão sair no rodapé -->
@@ -189,6 +190,7 @@ $recursos = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <a href="dashboard.php" class="rounded"><i class="fas fa-home"></i>&nbsp;&nbsp;Dashboard</a>
                     <a href="minhas_aulas.php" class="rounded"><i class="fas fa-calendar-alt"></i>&nbsp;&nbsp;&nbsp;Minhas Aulas</a>
                     <a href="recomendacoes.php" class="rounded active"><i class="fas fa-lightbulb"></i>&nbsp;&nbsp;&nbsp;Recomendações</a>
+                    <a href="anotacoes.php" class="rounded"><i class="fas fa-book-open"></i>&nbsp;&nbsp;&nbsp;Anotações</a>
                 </div>
 
                 <!-- Botão sair no rodapé -->
