d5511bb2c3283fdb8a089b5c2e782f96d875b820
Jorge <jorgeappontes13@gmail.com>
2026-04-29 20:46:33 -0300

    add link documentos

--- p/aluno/detalhes_aula.php
+++ p/aluno/detalhes_aula.php
@@ -1046,6 +1046,7 @@ $total_notificacoes_nao_lidas = $stmt_notif->fetch(PDO::FETCH_ASSOC)['total'];
                     <a href="minhas_aulas.php" class="rounded active"><i class="fas fa-calendar-alt"></i>&nbsp;&nbsp;&nbsp;Minhas Aulas</a>
                     <a href="recomendacoes.php" class="rounded"><i class="fas fa-lightbulb"></i>&nbsp;&nbsp;&nbsp;Recomendações</a>
                     <a href="anotacoes.php" class="rounded"><i class="fas fa-book-open"></i>&nbsp;&nbsp;&nbsp;Anotações</a>
+                    <a href="documentos.php" class="rounded"><i class="fa-solid fa-box-archive"></i>&nbsp;&nbsp;&nbsp;Documentos</a>
                 </div>
 
                 <div class="mt-auto">
