190015958e3315721995ee218a7f4e3228eb4090
Jorge <jorgeappontes13@gmail.com>
2026-06-08 13:19:27 -0300

    teste

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/index.css">
     <link rel="shortcut icon" href="<?php echo htmlspecialchars(get_setting('logo_image','LogoRisenglish.png')); ?>" type="image/x-icon">
-    
+    <!-- <p>teste</p> -->
 </head>
 <body>
     <!-- Navbar -->
